e41ad8781712e769f837915fe1173400d4e4cfd3
Nina <ninawang526@gmail.com>
2018-04-13 00:06:02 -0400

    updated scraper

---
+++ main.py
@@ -0,0 +1,11 @@
+import scraper
+import json
+
+scraper.getfriendsfollowers()
+
+# rtdata = open("sampledata.txt", "r").read()
+
+
+# rtjson = json.loads(rtdata)
+
+# scraper.interrelationships([rtjson])
